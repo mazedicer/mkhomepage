dbfcd30ab825a5a25fc6aff710a0134c186bdcd1
mazedicer <pcdoctormario@gmail.com>
2017-09-12 16:10:02 -0700

    cards, js, logo, fix

--- js/index.js
+++ js/index.js
@@ -18,17 +18,28 @@ $(document).ready(function(){
     
     //hide fixes
     var elements_to_hide_720 = [".carousel-info"];
+    var elements_to_hide_820 = ["#logo_div"];
     var elements_to_hide_1000 = ["h1"];
     $(window).on('resize', function(){
       hideFix();
+        logoFix();
     });
     hideFix();
+    logoFix();
     function hideFix(){
         elements_to_hide_720.map(hideShowElem_720);
+        elements_to_hide_820.map(hideShowElem_820);
         elements_to_hide_1000.map(hideShowElem_1000);
     }
     function hideShowElem_720(tag_elem){
         if ($(window).width() < 720){
+            $(tag_elem).hide();
+        }else{
+            $(tag_elem).show();
+        }
+    }
+    function hideShowElem_820(tag_elem){
+        if ($(window).width() < 820){
             $(tag_elem).hide();
         }else{
             $(tag_elem).show();
@@ -40,5 +51,10 @@ $(document).ready(function(){
         }else{
             $(tag_elem).show();
         }
+    }
+    //logo fix
+    function logoFix(){
+        var left_position = ($(window).width() / 2) - 100;
+        $("#logo_div").offset({left:left_position});
     }
 });
