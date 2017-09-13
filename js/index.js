b01ce307d2bdbee4b9258641f6a13ad0377e9169
mazedicer <pcdoctormario@gmail.com>
2017-09-12 18:10:48 -0700

    splash

--- js/index.js
+++ js/index.js
@@ -1,9 +1,57 @@
-$(document).ready(function(){
-    //homepage dropdown menu
+$(document).ready(function(){  
+    //load templates
+    //splash
+    fetch("./view/index/splash.html")
+    .then((resp)=>resp.text())
+    .then(function(data){
+        $("#splash_row").html(data);
+        //homepage dropdown menu
         $('.dropdown-item').on('click', function (e) {
             openSubMenu($(".dropdown-item").index(this));
             e.stopPropagation();
+        });
+    })
+    .catch(function(error){
+       alert(error); 
     });
+    //carousel
+    fetch("./view/index/carousel.html")
+    .then((resp)=>resp.text())
+    .then(function(data){
+        $("#carousel_row").html(data);
+        //hide fixes
+        $(window).on('resize', function(){
+            hideFix();
+            logoFix();
+        });
+        hideFix();
+        logoFix();
+    })
+    .catch(function(error){
+       alert(error); 
+    });
+    //our team
+    fetch("./view/index/our-team.html")
+    .then((resp)=>resp.text())
+    .then(function(data){
+        $("#our_team_row").html(data);
+    })
+    .catch(function(error){
+       alert(error); 
+    });
+    //cards
+    fetch("./view/index/card.html")
+        .then((resp)=>resp.text())
+        .then(function(data){
+            var replace = ["{img_source}","{t_name}","{t_description}"];
+            var replace_with = ["Mario", "John", "Dan"];
+            var my_card_template = data.replaceArray(replace, replace_with);
+            $("#our_team_cards_row").html(my_card_template);
+    })
+      .catch(function(error) {
+        alert(error);
+      }); 
+    
     function openSubMenu(elem){
         console.log(elem);
             switch(elem){
@@ -16,16 +64,9 @@ $(document).ready(function(){
                     }
     }
     
-    //hide fixes
     var elements_to_hide_720 = [".carousel-info"];
-    var elements_to_hide_820 = ["#logo_div"];
-    var elements_to_hide_1000 = ["h1"];
-    $(window).on('resize', function(){
-      hideFix();
-        logoFix();
-    });
-    hideFix();
-    logoFix();
+        var elements_to_hide_820 = ["#logo_div"];
+        var elements_to_hide_1000 = ["h1"];
     function hideFix(){
         elements_to_hide_720.map(hideShowElem_720);
         elements_to_hide_820.map(hideShowElem_820);
@@ -57,18 +98,6 @@ $(document).ready(function(){
         var left_position = ($(window).width() / 2) - 100;
         $("#logo_div").offset({left:left_position});
     }
-    //cards
-    var card_template = `<div class="col-md-3 col-sm-6">
-            <div class="card" style="width: 20rem;">
-              <img class="card-img-top" src="{img_source}" alt="Card image cap">
-              <div class="card-body">
-                <h4 class="card-title">{t_name}</h4>
-                <p class="card-text">{t_description}</p>
-              </div>
-                </div>
-            </div>`;
-    var replace = ["{img_source}","{t_name}","{t_description}"];
-    var replace_with = ["Mario", "John", "Dan"];
-    var my_card_template = card_template.replaceArray(replace, replace_with);
-    alert(my_card_template);
+    
+
 });
